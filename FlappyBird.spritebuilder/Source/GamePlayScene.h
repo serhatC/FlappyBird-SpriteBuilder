363e86806ce856eea74bdb609d6d3ba045bac500
serhatC <serhatakalin@SERHATs-MacBook-Pro.local>
2014-07-23 22:28:40 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.h
+++ FlappyBird.spritebuilder/Source/GamePlayScene.h
@@ -14,42 +14,13 @@ typedef NS_ENUM(NSInteger, DrawingOrder) {
     DrawingOrderGround,
     DrawingOrderHero
 };
-
 @interface GamePlayScene : CCNode <CCPhysicsCollisionDelegate>
 {
 // define variables here;
-    Character*     character;character = (Character*)[CCBReader load:@"Character"];romo
-    CCPhysicsNode* physicsNode;[physicsNode addChild:character];romo
+    Character*     character;
+    CCPhysicsNode* physicsNode;
+    float timeSinceObstacle;
 }
-
-// put new methods here
-- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
-    // this will get called every time the player touches the screen
-    -(type I return)nameOfMethod: (type of first parameter) firstParameter continuationOfMethodName: (type of second parameter) secondParameter
-    -(void) doSomething
-    {
-        NSLog(@"Hello World!");
-    }
-    
-    -(int) doSomethingWithAString: (NSString*) myString
-    {
-        //returns the length of the string times ten
-        return [myString length] * 10;
-    }
-    
-    -(NSArray *) addThisStringToAnArray:(NSString*) firstString andThisString:(NSString*) secondString andAlsoThisString:(NSString*) thirdString
-    {
-        return [NSArray arrayWithObjects: firstString, secondString, thirdString, nil];
-    }
-    
-    // touches the screen
-    [character flap];
-    
-}
-
-
-
-
 -(void) initialize;
 -(void) addObstacle;
 -(void) showScore;
